4fc7ff06c0c7ed8b92546c8c7de560f3dcd7e905
sCherry1101 <cherry.1101.8113@gmail.com>
2026-08-01 17:50:45 +0530

    v0:var

---
+++ build/CMakeFiles/Persephone.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Persephone.dir/link.d"
+  "CMakeFiles/Persephone.dir/src/main.cpp.o"
+  "CMakeFiles/Persephone.dir/src/main.cpp.o.d"
+  "Persephone"
+  "Persephone.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Persephone.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
