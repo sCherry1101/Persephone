b31a125a6f1b75cb32c94c4e322815d7addfe146
sCherry1101 <cherry.1101.8113@gmail.com>
2026-08-02 16:01:27 +0530

    fix: cmake file rename and update

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(){
+    std::cout << "========================\n";
+    std::cout << "Persephone\n";
+    std::cout << "========================\n";
+    std::cout << "Loading...\n";
+    std::cout << "Welcome! Enter your name:";
+    std::string name;
+    std::cin >> name;
+    std::cout << "hello " << name << "\n";
+    std::cout << "========================\n";
+    std::cout << "Press enter to continue...";
+    return 0;
+}
+
+
+//i have ZERO c++ experience so the first few commits ?? yes its gonna be like this and i learn by making projects 
