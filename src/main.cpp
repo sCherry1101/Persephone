d13ec3007d5a281c224c844998e71f8c30907441
sCherry1101 <cherry.1101.8113@gmail.com>
2026-08-05 16:07:46 +0530

    add: improved ui?

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,26 @@
 #include <iostream>
 
 int main(){
-    std::cout << "========================\n";
-    std::cout << "Persephone\n";
-    std::cout << "========================\n";
+    std::cout << "╔════════════════════════════╗\n";
+    std::cout << "║       Persephone           ║\n";
+    std::cout << "╚════════════════════════════╝\n";
     std::cout << "Loading...\n";
     std::cout << "Welcome! Enter your name:";
     std::string name;
     std::cin >> name;
-    std::cout << "hello " << name << "\n";
-    std::cout << "========================\n";
-    std::cout << "Press enter to continue...";
+    std::cout << "Hello " << name << "\n";
+    std::cout << "╔════════════════════════════╗\n";
+    std::cout << "║  Press enter to continue...║\n";
+    std::cout << "╚════════════════════════════╝\n";
+    std::cout << "║           Menu             ║\n";
+    std::cout << "╠════════════════════════════╣\n";
+    std::cout << "║ 1. Login to spotify        ║\n";
+    std::cout << "║ 2. Current Song            ║\n";
+    std::cout << "║ 3. Play/Pause              ║\n";
+    std::cout << "║ 4. Next Song               ║\n";
+    std::cout << "║ 5. Previous Song           ║\n";
+    std::cout << "║ 6. Exit                    ║\n";
+    std::cout << "╚════════════════════════════╝\n";
     return 0;
 }
 
