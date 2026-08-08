47ad2c3050d2a28d3bb53b63bbf8d50357b99890
sCherry1101 <cherry.1101.8113@gmail.com>
2026-08-08 18:11:42 +0530

    fix : the layout

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,63 @@
 #include <iostream>
+#include <string>
 
 int main(){
+
     std::cout << "╔════════════════════════════╗\n";
     std::cout << "║       Persephone           ║\n";
     std::cout << "╚════════════════════════════╝\n";
     std::cout << "Loading...\n";
     std::cout << "Welcome! Enter your name:";
+
     std::string name;
     std::cin >> name;
     std::cout << "Hello " << name << "\n";
+
+    int choice=0;
+
+    while (choice !=6) {
     std::cout << "╔════════════════════════════╗\n";
     std::cout << "║  Press enter to continue...║\n";
     std::cout << "╚════════════════════════════╝\n";
+    std::cout << "╔════════════════════════════╗\n";
     std::cout << "║           Menu             ║\n";
     std::cout << "╠════════════════════════════╣\n";
     std::cout << "║ 1. Login to spotify        ║\n";
-    std::cout << "║ 2. Current Song            ║\n";
-    std::cout << "║ 3. Play/Pause              ║\n";
-    std::cout << "║ 4. Next Song               ║\n";
-    std::cout << "║ 5. Previous Song           ║\n";
-    std::cout << "║ 6. Exit                    ║\n";
+    std::cout << "║ 2. Profile                 ║\n";
+    std::cout << "║ 3. Themes                  ║\n";
+    std::cout << "║ 4. Commands                ║\n";
+    std::cout << "║ 5. Exit                    ║\n";
+    std::cout << "║ 6. Log out                 ║\n";
     std::cout << "╚════════════════════════════╝\n";
-    return 0;
-}
+    
+    std::cout << "Choice:";
+    std::cin  >> choice;
 
+    if (choice==1){
+        std::cout<< "Login selected\n";
+    }
+    else if (choice==2){
+        std::cout<< "Profile selected\n";
+    }
+    else if (choice==3){
+        std::cout<< "Themes selected\n";
+    }
+    else if (choice==4){
+        std::cout<< "Commands selected\n";
+    }
+    else if (choice==5){
+        std::cout<< "Exit selected\n";
+    }
+    else if (choice==6){
+        std::cout<< "Log out selected\n";
+    }
+    else{
+        std::cout<< "Invalid choice, please try again.\n";
+    }
+
+}
+    return 0;
+
+}
 
 //i have ZERO c++ experience so the first few commits ?? yes its gonna be like this and i learn by making projects 
